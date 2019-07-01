6b0c0f4ce5a820af5e7e949dccfe55d4b482d74b
Nick <nickpinchuk91@gmail.com>
2019-07-01 17:53:22 +0300

    NL-4

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php
 
 $mapping = array(
-    '/learning/item-array' => 'learning_item/array.php',
-    '/learning/item-functions' => 'functions.php',
-    '/learning/item-cycles' => 'cycles.php'
+    '/learning/item-array' =>
+        array('learning_item/array.php', 'Array'),
+    '/learning/item-functions' =>
+        array('learning_item/functions.php', 'functions'),
+    '/learning/item-cycles' =>
+        array('learning_item/cycles.php', 'cycles'),
+    '/learning/item-files' =>
+        array('learning_item/Files.php', 'Files'),
+    '/learning/item-comment' =>
+        array('learning_item/Comment.php', 'Comment'),
+    '/learning/item-forms' =>
+        array('learning_item/forms.php', 'Forms')
 );
 
 $uri = $_SERVER['REQUEST_URI'];
-
 if (isset($mapping[$uri])) {
-    include $mapping[$uri];
+    $a = $mapping[$uri];
+    include $mapping[$uri][0];
 }
 else {
     ?>
@@ -21,7 +30,7 @@ else {
         foreach ($mapping as $uri => $path) {
             ?>
             <tr>
-                <td><a href=<?php echo $uri; ?>> Lessen</a></td>
+                <td><a href=<?php echo $uri; ?>><?php echo $path[1]; ?></a></td>
             </tr>
             <?php
         }
